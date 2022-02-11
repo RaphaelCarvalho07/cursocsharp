480b84b9bd86aae8eec12945765c58cda67cf0bb
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-11 12:26:48 -0300

    Commit inicial Projeto Aprendizado C#

---
+++ Fundamentos/Comentarios.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentarios
+    {
+        public static void Executar() {
+            // Cuidado com os cuidados desnecessários
+            // Console.WriteLine quebra linha
+            // Console.Write mantém na mesma linha
+            Console.WriteLine("Código claro é sempre melhor");
+
+            Console.WriteLine("O C# tem XML Comments");
+        }
+    }
+}
---
+++ Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+       public static void Executar() {
+            // Console.WriteLine quebra linha
+            // Console.Write mantém na mesma linha          
+            Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
