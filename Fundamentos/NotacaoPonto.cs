06f8596e574b87993901c3aaafb7474878840f41
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-12 09:44:25 -0300

    Aula 3.11 - Notação Ponto

---
+++ Fundamentos/NotacaoPonto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class NotacaoPonto
+    {
+        public static void Executar()
+        {
+            var saudacao = "olá".ToUpper().Insert(3, " World!")
+                .Replace("World!", "Mundo!");
+
+            Console.WriteLine(saudacao);
+
+            Console.WriteLine("Teste".Length);
+            /* Notação em nulo causa erro.Use navegação segura,
+            representado por sinal ? para conferir o conteúdo.
+            Isso é chamado de "Navegação Segura"
+            */
+            string valorImportante = null;
+            Console.WriteLine(valorImportante?.Length);
+        }
+    }
+}
