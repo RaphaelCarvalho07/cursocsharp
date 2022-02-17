c839b9babbf0b19e19c55020998447c6f55d85e1
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-17 18:17:51 -0300

    Aula 55 - Params - Classes e Métodos

---
+++ ClassesEMetodos/Params.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Params
+    {
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"Olá {pessoa}");
+            }
+        }
+
+        public static void Executar()
+        {
+            Recepcionar("Pedro", "Manu", "Roger", "Ana", "Bia");
+        }
+    }
+}
