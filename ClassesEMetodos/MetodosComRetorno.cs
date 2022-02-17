97357d5ee241f7f6a2648b8e6844f084b13e81d0
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-17 12:32:32 -0300

    Aula 50 - Métodos Com Retorno #2 - Clases e Métodos

--- ClassesEMetodos/MetodosComRetorno.cs
+++ ClassesEMetodos/MetodosComRetorno.cs
@@ -26,6 +26,40 @@ namespace CursoCSharp.ClassesEMetodos
         }
     }
 
+    class CalculadoraCadeia
+    {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a)
+        {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a)
+        {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar()
+        {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado()
+        {
+            return memoria;
+        }
+    }
+
     class MetodosComRetorno
     {
         public static void Executar()
@@ -35,9 +69,15 @@ namespace CursoCSharp.ClassesEMetodos
             var resultado2 = CalculadoraComum.Dividir(10, 5);
 
             Console.WriteLine(resultado);
-            Console.WriteLine(CalculadoraComum.Subtrair(2, 7));
-            Console.WriteLine(CalculadoraComum.Multiplicar(10, 10));
             Console.WriteLine(resultado2);
+            Console.WriteLine(CalculadoraComum.Subtrair(2, 7));
+            Console.WriteLine(CalculadoraComum.Multiplicar(4, 4));
+
+            var CalculadoraCadeia = new CalculadoraCadeia();
+            CalculadoraCadeia.Somar(3).Multiplicar(3).Imprimir().Limpar().Imprimir();
+
+            resultado = CalculadoraCadeia.Somar(3).Multiplicar(2).Resultado();
+            Console.WriteLine(resultado);
         }
     }
 }
