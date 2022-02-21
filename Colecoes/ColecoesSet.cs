b688f79ae7b1f328b0c53154a3037826c68dc007
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-21 18:48:53 -0300

    Aula 70 - Set - Coleções

---
+++ Colecoes/ColecoesSet.cs
@@ -0,0 +1,41 @@
+/* 
+Sets não são indexados e Set não aceita repetição
+ */
+using System;
+using System.Collections.Generic;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesSet
+    {
+        public static void Executar()
+        {
+            var livro = new Produto("Game of Thrones", 49.9);
+
+            var carrinho = new HashSet<Produto>();
+            carrinho.Add(livro);
+
+            var combo = new HashSet<Produto>
+            {
+                new Produto("Camisa", 29.9),
+                new Produto("8ª Temporada Game of Thrones", 99.9),
+                new Produto("Poster", 10)
+            };
+
+            carrinho.UnionWith(combo);
+            Console.WriteLine(carrinho.Count);
+            // carrinho.RemoveAt(3);
+            
+            foreach (var item in carrinho)
+            {
+                // Console.Write($"Índice do item {carrinho.IndexOf(item)},");
+                Console.WriteLine($" Nome => {item.Nome}, Preço => {item.Preco}");
+            }
+
+            Console.WriteLine($"Antes de tentar adicionar o livro => {carrinho.Count}");
+            carrinho.Add(livro);
+            Console.WriteLine($"Depois de tentar adicionar o livro => {carrinho.Count}");
+            // Console.WriteLine(carrinho.LastIndexOf(livro));
+        }
+    }
+}
