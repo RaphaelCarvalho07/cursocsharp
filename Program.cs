e7099bcf8ce58f350515479f61b836393c4d5e34
Raphael Carvalho <raphilskerj@gmail.com>
2022-02-22 18:17:22 -0300

    Aula 74 - Dictionary - Coleções

--- Program.cs
+++ Program.cs
@@ -69,6 +69,7 @@ namespace CursoCSharp
                 {"Queue - Coleções", ColecoesQueue.Executar},
                 {"Igualdade - Coleções", Igualdade.Executar},
                 {"Stack - Coleções", ColecoesStack.Executar},
+                {"Dictionary - Coleções", ColecoesDictionary.Executar},
         });
 
             central.SelecionarEExecutar();
