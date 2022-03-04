50019e73f39c39dbfc7bb0e09763de96510db48e
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-04 18:58:09 -0300

    Aula 87 - Exemplo Lambda - Métodos & Funções

---
+++ CursoCSharp/MetodosEFuncoes/ExemploLambda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.MetodosEFuncoes
+{
+    class ExemploLambda
+    {
+        public static void Executar()
+        {
+            Action algoNoConsole = () =>
+            {
+                Console.WriteLine("Lambda com C#!");
+            };
+            algoNoConsole();
+
+            Func<int> jogarDado = () =>
+            {
+                Random random = new Random();
+                return random.Next(1, 7);
+            };
+
+            Console.WriteLine(jogarDado());
+
+
+            /*
+              A função abaixo poderia ser representada da seguinte forma:
+              Func<int, string> conversorHex = (numero) =>
+            {
+                return numero.ToString("X");
+            };
+            */
+            Func<int, string> conversorHex = numero => numero.ToString("X");
+            Console.WriteLine(conversorHex(1234));
+
+            Func<int, int, int, string> formatarData = (dia, mes, ano) => String.Format($"{dia:D2}/{mes:D2}/{ano:D4}");
+            Console.WriteLine(formatarData(1, 1, 2019));
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -6,6 +6,7 @@ using CursoCSharp.EstruturasDeControle;
 using CursoCSharp.ClassesEMetodos;
 using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
+using CursoCSharp.MetodosEFuncoes;
 
 namespace CursoCSharp
 {
@@ -80,6 +81,9 @@ namespace CursoCSharp
                 {"Abstract - OO",  Abstract.Executar},
                 {"Interface - OO",  Interface.Executar},
                 {"Sealed - OO",  Sealed.Executar},
+
+                // Métodos & Funções
+                {"ExemploLambda - OO",  ExemploLambda.Executar},
         });
 
             central.SelecionarEExecutar();
